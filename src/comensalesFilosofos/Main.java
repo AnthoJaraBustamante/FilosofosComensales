b31228fda36a775a98c7d8a6b0f16a2d2cb56545
antho <antho@OMEN-HP-Laptop>
2018-05-12 01:45:54 -0500

    First Commit

---
+++ src/comensalesFilosofos/Main.java
@@ -0,0 +1,21 @@
+package comensalesFilosofos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Accion c = new Accion(3); 
+        Filosofo f[] = new Filosofo[3];
+
+        for (int cont = 0; cont < 3; cont++) {
+            f[cont] = new Filosofo(cont, c);
+            f[cont].start();
+        }
+
+        try {
+            Thread.sleep((int) (8000));
+        } catch (InterruptedException ex1) {
+            System.out.println(ex1);
+        }
+        System.exit(0);
+    }
+}
