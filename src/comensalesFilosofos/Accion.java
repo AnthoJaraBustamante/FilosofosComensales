aac97a1425412963728b9d3e5c17d2ccd2b74851
antho <antho@OMEN-HP-Laptop>
2018-05-12 01:46:14 -0500

    Commit

--- src/comensalesFilosofos/Accion.java
+++ src/comensalesFilosofos/Accion.java
@@ -2,56 +2,56 @@ package comensalesFilosofos;
 
 public class Accion {
 
-    private static final int piensa = 0;
-    private static final int come = 2;
-    private static final int hambre = 1;
+	private static final int piensa = 0;
+	private static final int come = 2;
+	private static final int hambre = 1;
 
-    private int n;
-    private int estado[];
+	private int n;
+	private int estado[];
 
-    public Accion(int n) {
-        this.n = n;
-        estado = new int[n];
-    }
+	public Accion(int n) {
+		this.n = n;
+		estado = new int[n];
+	}
 
-    public synchronized void tomarTenedores(int id) {
+	public synchronized void tomarTenedores(int id) {
 
-        estado[id] = hambre;
-        prueba(id);
+		estado[id] = hambre;
+		prueba(id);
 
-        while (estado[id] != come)
-        {
-            try {
-                wait();
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-        }
-    }
+		while (estado[id] != come)
+		{
+			try {
+				wait();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+		}
+	}
 
-    public synchronized void soltarTenedores(int id) {
-        int der, izq;
-        der = (id + 1) % n;
-        izq = id - 1;
-        if (izq < 0) {
-            izq = n - 1;
-        }
+	public synchronized void soltarTenedores(int id) {
+		int der, izq;
+		der = (id + 1) % n;
+		izq = id - 1;
+		if (izq < 0) {
+			izq = n - 1;
+		}
 
-        estado[id] = piensa;
-        prueba(izq);
-        prueba(der);
-    }
+		estado[id] = piensa;
+		prueba(izq);
+		prueba(der);
+	}
 
-    public void prueba(int id) {
-        int der, izq;
-        der = (id + 1) % n;
-        izq = id - 1;
-        if (izq < 0) {
-            izq = n - 1;
-        }
-        if (estado[id] == hambre && estado[izq] != come && estado[der] != come) {
-            estado[id] = come;
-            notifyAll();
-        }
-    }
+	public void prueba(int id) {
+		int der, izq;
+		der = (id + 1) % n;
+		izq = id - 1;
+		if (izq < 0) {
+			izq = n - 1;
+		}
+		if (estado[id] == hambre && estado[izq] != come && estado[der] != come) {
+			estado[id] = come;
+			notifyAll();
+		}
+	}
 }
--- src/comensalesFilosofos/Filosofo.java
+++ src/comensalesFilosofos/Filosofo.java
@@ -2,41 +2,40 @@ package comensalesFilosofos;
 
 public class Filosofo extends Thread {
 
-    private int id;
-    private Accion controlador;
-    private int nComidas;
+	private int id;
+	private Accion controlador;
+	private int nComidas;
 
-    public Filosofo(int id, Accion c) {
-        this.id = id;
-        controlador = c;
-        nComidas = 0;
-    }
-    
-    public void pensando() {
-        System.out.println("Filosofo " + id + " pensando");
-        try {
-            Thread.sleep((int) (Math.random() * 400));
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
-    public void comer() {
-        nComidas++;
-        System.out.println("Filosofo " + id + " come: " + nComidas + " veces");
-        try {
-            Thread.sleep((int) (Math.random() * 500));
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+	public Filosofo(int id, Accion c) {
+		this.id = id;
+		controlador = c;
+		nComidas = 0;
+	}
 
-    public void run() {
-        do {
-            pensando();
-            controlador.tomarTenedores(id);
-            comer();
-            controlador.soltarTenedores(id);
-        } while (true);
-    }
+	public void pensando() {
+		System.out.println("Filosofo " + id + " pensando");
+		try {
+			Thread.sleep((int) (Math.random() * 400));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+	public void comer() {
+		nComidas++;
+		System.out.println("Filosofo " + id + " come: " + nComidas + " veces");
+		try {
+			Thread.sleep((int) (Math.random() * 500));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 
+	public void run() {
+		do {
+			pensando();
+			controlador.tomarTenedores(id);
+			comer();
+			controlador.soltarTenedores(id);
+		} while (true);
+	}
 }
